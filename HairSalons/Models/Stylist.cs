7e8f557eccfabd4c19cf81f3f6b2d2529813b55a
Brit Wallace <britwallace888@gmail.com>
2022-03-12 20:51:15 -0800

    Add Models

--- HairSalons/Models/Stylist.cs
+++ HairSalons/Models/Stylist.cs
@@ -1,1 +1,19 @@
 
+using System.Collections.Generic;
+
+namespace HairSalons.Models
+{
+  public class Stylist
+  {
+    public Stylist()
+    {
+      this.Clients = new HashSet<Client>();
+    }
+
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+
+    public string Specialty { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
+  }
+}
